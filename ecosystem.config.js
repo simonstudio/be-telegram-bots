3942c6152c7bc23e719796557faddf6b4eda4fb8
Simon <simon72217@gmail.com>
2023-09-25 17:55:29 +0700

    a

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "apps": [
+        // 1
+        {
+            name: "metafanpagesupport.pro",
+            cwd: "../exins/build/metafanpagesupport.pro",
+            script: "npx",
+            args: "serve -s metafanpagesupport.pro -p " + 3001
+        },
+
+        {
+            name: "api.metafanpagesupport.pro",
+            cwd: "./be-telegram-bots",
+            script: "npm",
+            args: "start",
+            env: {
+                PORT: 3002,
+                MONGO_NAME: "metafanpagesupportpro",
+                TELEGRAM_ROOM: "-4041758075", 
+                TELEGRAM_BOT: "6438110441:AAFCZPL9w5jWGJBdKwqMpDsPZvHf4vT8jqQ"
+            }
+        },
+
+    ]
+}
